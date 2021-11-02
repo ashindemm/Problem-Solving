f0f5049a0b512353d6b2e62dde6181a6888f6f87
Akshay Shinde <akshay.r.shinde2696@gmail.com>
2021-11-01 18:36:37 -0700

    Time: 5 ms (85.57%), Space: 38.4 MB (96.74%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.java
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,27 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals == null || intervals.length == 0) {
-            return 0;
-        }
-        PriorityQueue<Integer> pq = new PriorityQueue();
         Arrays.sort(intervals, new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
-        int rooms = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
         for (int[] interval: intervals) {
-            if (pq.size() == 0) {
-                rooms ++;
-            } else {
-                if (interval[0] >= pq.peek()) {
+            if (!pq.isEmpty()) {
+                if (pq.peek() <= interval[0]) {
                     pq.poll();
-                } else {
-                    rooms ++;
                 }
             }
             pq.add(interval[1]);
         }
+        
         return pq.size();
     }
 }
