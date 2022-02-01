7cb9d0f311188fcf1d9cd5b7122a2f786c48e92e
Akshay Shinde <akshay.r.shinde2696@gmail.com>
2022-02-01 01:31:34 -0800

    Time: 3 ms (92.06%), Space: 71.5 MB (71.79%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -3,16 +3,15 @@ class Solution {
         if (trust.length < n - 1)
             return -1;
 
-        int[] indegree = new int[n+1];
-        int[] outdegree = new int[n+1];
+        int[] degreeSum = new int[n+1];
         
         for (int[] t: trust) {
-            indegree[t[1]]++;
-            outdegree[t[0]]++;
+            degreeSum[t[1]]++;
+            degreeSum[t[0]]--;
         }
         
         for (int i = 1; i <= n; i++) {
-            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+            if (degreeSum[i] == n - 1) {
                 return i;
             }
         }
