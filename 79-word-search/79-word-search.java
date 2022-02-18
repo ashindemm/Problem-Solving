5ee7f29b78338181de641cd9736cba451cd4b909
Akshay Shinde <akshay.r.shinde2696@gmail.com>
2022-02-17 19:56:59 -0800

    Time: 143 ms (61.27%), Space: 42.7 MB (19.96%) - LeetHub

---
+++ 79-word-search/79-word-search.java
@@ -0,0 +1,35 @@
+class Solution {
+    char[][] boardCopy;
+    public boolean exist(char[][] board, String word) {
+        boardCopy = board;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backtrack(i, j, board, word, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean backtrack(int row, int col, char[][] board, String word, int posi) {
+        if (word.length() <= posi)
+            return true;
+        
+        if (row < 0 || row == boardCopy.length || col < 0 || col == boardCopy[0].length || board[row][col] != word.charAt(posi))
+            return false;
+        
+        board[row][col] = '#';
+        boolean exists = false;
+        if (!exists)
+            exists = backtrack(row + 1, col, board, word, posi + 1);
+        if (!exists)
+            exists = backtrack(row, col + 1, board, word, posi + 1);
+        if (!exists)
+            exists = backtrack(row - 1, col, board, word, posi + 1);
+        if (!exists)
+            exists = backtrack(row, col - 1, board, word, posi + 1);
+        
+        board[row][col] = word.charAt(posi);
+        return exists;
+    }
+}
