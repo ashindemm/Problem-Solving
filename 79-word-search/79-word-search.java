f782aca9bab4a1ece6a35a48451d56a808c350ea
Akshay Shinde <akshay.r.shinde2696@gmail.com>
2022-02-17 19:57:14 -0800

    Time: 97 ms (86.76%), Space: 40.3 MB (44.00%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,7 +1,5 @@
 class Solution {
-    char[][] boardCopy;
     public boolean exist(char[][] board, String word) {
-        boardCopy = board;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (backtrack(i, j, board, word, 0))
@@ -15,7 +13,7 @@ class Solution {
         if (word.length() <= posi)
             return true;
         
-        if (row < 0 || row == boardCopy.length || col < 0 || col == boardCopy[0].length || board[row][col] != word.charAt(posi))
+        if (row < 0 || row == board.length || col < 0 || col == board[0].length || board[row][col] != word.charAt(posi))
             return false;
         
         board[row][col] = '#';
